422b6336ef93c1ba65b5e9a5fea834593e5b2ca7
Akriti <akritiomishra@gmail.com>
2026-09-19 00:21:51 +0530

    User Module

---
+++ src/com/company/ithelpdesk/model/user/User.java
@@ -0,0 +1,30 @@
+package com.company.ithelpdesk.model.user;
+
+import com.company.ithelpdesk.enums.UserRole;
+
+public abstract class User {
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private boolean active;
+
+    public User(long id, String name, String email, String phone, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.active = active;
+    }
+
+    public abstract UserRole getRole();
+
+    public void displayProfile(){
+        System.out.println("Id:"+ id);
+        System.out.println("Name:"+ name);
+        System.out.println("Email:"+ email);
+        System.out.println("Phone:"+ phone);
+        System.out.println("Active:"+ active);
+
+    }
+}
